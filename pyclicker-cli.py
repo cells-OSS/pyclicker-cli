4e4daee65d175d5d9cf2cea083fabba504501ba1
cells <open-source-version@proton.me>
2025-11-22 17:17:35 +0300

    added functions for right clicking

--- pyclicker-cli.py
+++ pyclicker-cli.py
@@ -8,20 +8,37 @@ import threading
 mouse_controller = mouse.Controller()
 clicking = False
 
-def click_loop():
-    """Loop that clicks while 'clicking' is True."""
+def click_left():
     while clicking:
         mouse_controller.click(mouse.Button.left)
         time.sleep(delay)
 
-def on_press(key):
+def click_right():
+    while clicking:
+        mouse_controller.click(mouse.Button.right)
+        time.sleep(delay)
+
+def on_press_left(key):
     global clicking
     try:
         if key == keyboard.Key.insert:
             clicking = not clicking
             if clicking:
                 print("Clicking started")
-                threading.Thread(target=click_loop, daemon=True).start()
+                threading.Thread(target=click_left, daemon=True).start()
+            else:
+                print("Clicking stopped")
+    except AttributeError:
+        pass
+
+def on_press_right(key):
+    global clicking
+    try:
+        if key == keyboard.Key.insert:
+            clicking = not clicking
+            if clicking:
+                print("Clicking started")
+                threading.Thread(target=click_right, daemon=True).start()
             else:
                 print("Clicking stopped")
     except AttributeError:
@@ -42,5 +59,5 @@ delay = int(input("Type the delay you want in seconds: "))
 print(f"The clicker is set to be ran at {delay} seconds.")
 print("Clicking will now start/stop when you press the 'insert' key...")
 
-with keyboard.Listener(on_press=on_press) as listener:
+with keyboard.Listener(on_press=on_press_left) as listener:
     listener.join()
